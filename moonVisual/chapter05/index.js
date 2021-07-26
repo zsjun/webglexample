2ad4eae7e235bceb868a85a33a7b450859412f26
zsj <zousjun@126.com>
2021-07-26 10:20:41 +0800

    add

--- moonVisual/chapter05/index.js
+++ moonVisual/chapter05/index.js
@@ -15,28 +15,26 @@ function regularShape(edges = 3, x, y, step) {
 // draw(regularShape(11, -64, -64, 30)); // 绘制十一边形
 // draw(regularShape(60, 128, -64, 6));
 
-function draw(context, v) {
-  console.log(v);
-  for (let i = 0; i < v.length; i++) {
-    // ctx.beginPath();
-    ctx.moveTo(v[i][0], v[i][1]);
-    if (i + 1 < v.length) {
-      ctx.lineTo(v[i + 1][0], v[i + 1][1]);
-    }
-    ctx.stroke();
-    // ctx.closePath();
+function drawBranch(context, v0, length, thickness, dir, bias) {
+  const v = new Vector2D(1, 0).rotate(dir).scale(length);
+  // console.log(v);
+  const v1 = v0.copy().add(v);
+  ctx.beginPath();
+  ctx.moveTo(v0[0], v0[1]);
+  ctx.lineTo(v1[0], v1[1]);
+  ctx.stroke();
+  if (thickness > 2) {
+    const left = dir + 0.2;
+    drawBranch(context, v1, length * 0.9, thickness * 0.8, left, bias * 0.9);
+    const right = dir - 0.2;
+    drawBranch(context, v1, length * 0.9, thickness * 0.8, right, bias * 0.9);
   }
 }
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-// ctx.translate(canvas.width / 2, canvas.height / 2);
-// ctx.scale(1, -1);
+ctx.translate(0, canvas.height);
+ctx.scale(1, -1);
 ctx.lineCap = 'round';
-// const ret = regularShape(3, 128, 128, 100);
-// draw(ctx, ret);
-const ret = regularShape(6, -64, 128, 50);
-draw(ctx, ret); // 绘制六边形
-// draw(ctx, regularShape(11, -64, -64, 30)); // 绘制十一边形
-// draw(ctx, regularShape(60, 128, -64, 6));
 // ctx.fillStyle = 'green';
 // ctx.fillRect(10, 10, 150, 100);
+drawBranch(ctx, new Vector2D(150, 1), 100, 10, 1.6, 1);
